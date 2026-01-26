cff02c681edca0c7f85f006011f4b2ee51e317d5
Carlo James Polancos <carlo.polancos@awsys-i.com>
2026-01-26 11:04:43 +0800

    committing changes after completing section12

--- src/main/java/browser/BrowserManager.java
+++ src/main/java/browser/BrowserManager.java
@@ -3,8 +3,16 @@ package browser;
 import com.microsoft.playwright.*;
 
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BrowserManager {
 
@@ -12,6 +20,20 @@ public class BrowserManager {
     public Browser browser;
     public BrowserContext browserContext;
     public Page page;
+    public Properties properties;
+    private static final Logger logger = Logger.getLogger(BrowserManager.class.getName());
+
+    public BrowserManager() {
+        properties = new Properties();
+        Path configPath = Paths.get(System.getProperty("config.path",
+                Paths.get(System.getProperty("user.dir"), "src", "main", "resources",
+                        "config.properties").toString()));
+        try (InputStream in = Files.newInputStream(configPath)) {
+            properties.load(in);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error loading config file.", e);
+        }
+    }
 
     public byte[] takeScreenshot() {
         if (page != null) {
@@ -21,7 +43,7 @@ public class BrowserManager {
     }
 
     public void setUp() {
-        System.out.println("Setting up Playwright...");
+        logger.info("Setting up Playwright...");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int) screenSize.getWidth();
         int height = (int) screenSize.getHeight();
@@ -29,14 +51,26 @@ public class BrowserManager {
         arguments.add("--start-maximized");
 
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(arguments));
+        String browserType = properties.getProperty("browser", "chromium");
+        switch (browserType.toLowerCase()) {
+            case "chromium":
+                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(arguments));
+                break;
+            case "firefox":
+                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(arguments));
+                break;
+            default:
+                logger.warning("Unknown browser type: " + browserType + ". Defaulting to chromium.");
+                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(arguments));
+                break;
+        }
         browserContext = browser.newContext(new Browser.NewContextOptions().setViewportSize(null));
         page  = browserContext.newPage();
-        System.out.println("Playwright setup complete");
+        logger.info("Playwright setup complete");
     }
 
     public void tearDown() {
-        System.out.println("Tearing down Playwright...");
+        logger.info("Tearing down Playwright...");
         try {
             if (playwright != null) {
                 playwright.close();
@@ -44,6 +78,6 @@ public class BrowserManager {
         } catch (Exception e) {
             System.err.println("Error during teardown: " + e.getMessage());
         }
-        System.out.println("Playwright teardown complete");
+        logger.info("Playwright teardown complete");
     }
 }
